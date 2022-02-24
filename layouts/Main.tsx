784ce73337a3399a39c50890627f7ce9dade91a1
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2022-02-24 18:27:32 +0100

    fix: removed isDarkMode prop

--- layouts/Main.tsx
+++ layouts/Main.tsx
@@ -1,20 +1,17 @@
 import React, { ReactElement } from 'react'
 import Navigation from '../components/Navigation'
 import { Footer } from '../components/Footer'
-import useDarkMode from 'use-dark-mode'
 
 type Props = {
   children: ReactElement | ReactElement[]
 }
 
 function MainLayout({ children }: Props) {
-  const darkMode = useDarkMode(false, { classNameDark: 'dark-mode' })
-
   return (
     <>
       <Navigation />
       <main style={{ position: 'relative' }}>{children}</main>
-      <Footer isDarkMode={darkMode.value} toggleDarkMode={darkMode.toggle} />
+      <Footer />
     </>
   )
 }
